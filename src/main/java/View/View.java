cfb59153ef58d4a5deb43842be12954d76459b6b
John Afolayan <johnafolayan11@yahoo.ca>
2020-11-11 00:06:31 -0500

    Added move functionality. Player can move troops before and after attack to a country which they own. Changed few variable names for easier readablilty.

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -34,10 +34,10 @@ public class View extends JFrame implements ModelUpdateListener {
     JButton attackButton;
     JButton passTurnButton;
     JButton quitButton;
+    JButton moveButton;
     ArrayList<JButton> listOfCommandButtons;
     ArrayList<CircleButton> listOfCountryButtons;
     JTextArea feedbackArea;
-    JButton moveButton;
     Map<String, CircleButton> mapOfButtons = new HashMap<>();
     CircleButton alaska;
     CircleButton alberta;
@@ -398,6 +398,10 @@ public class View extends JFrame implements ModelUpdateListener {
 
     public JButton getNewGameButton() {
         return newGameButton;
+    }
+
+    public JButton getMoveButton() {
+        return moveButton;
     }
 
     public int numberOfPlayersRequest() {
